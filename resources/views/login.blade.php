16f0d250d31e81f9fc8efa7cc0adab7bd691d57e
beeerlian <naufal.berlian.99@gmail.com>
2021-12-17 16:19:38 +0700

    add: login register ui

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,7 @@
 
 <head>
 
-  <title>Digital Trend HTML Template</title>
+  <title>Sign In</title>
   <!--
 
 DIGITAL TREND
@@ -56,10 +56,9 @@ https://templatemo.com/tm-538-digital-trend
   <!-- MENU BAR -->
   <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand" href="index.html">
-        <i class="fa fa-line-chart"></i>
-        Digital Trend
-      </a>
+      <p class="navbar-brand" >
+        E-Learning - Sign In
+      </p>
 
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -68,16 +67,7 @@ https://templatemo.com/tm-538-digital-trend
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a href="#about" class="nav-link smoothScroll">Studio</a>
-          </li>
-          <li class="nav-item">
-            <a href="#project" class="nav-link smoothScroll">Our Works</a>
-          </li>
-          <li class="nav-item">
-            <a href="blog.html" class="nav-link">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a href="contact.html" class="nav-link contact">Contact</a>
+            <a href="/register" class="nav-link contact">Sign Up</a>
           </li>
         </ul>
       </div>
@@ -138,7 +128,7 @@ https://templatemo.com/tm-538-digital-trend
 
             <h2 class="mb-4" data-aos="fade-up">the best <strong>Digital Marketing agency</strong> in Rio de Janeiro
             </h2>
-
+            
             <p class="mb-0" data-aos="fade-up">Total 5 HTML pages are included in this template from TemplateMo website.
               Please check 2 <a href="blog.html">blog</a> pages, <a href="project-detail.html">project</a> page, and <a href="contact.html">contact</a> page.
               <br><br>You are <strong>allowed</strong> to use this template for commercial or non-commercial purpose.
@@ -155,102 +145,6 @@ https://templatemo.com/tm-538-digital-trend
       </div>
     </div>
   </section>
-
-
-  <!-- PROJECT -->
-  <section class="project section-padding" id="project">
-    <div class="container-fluid">
-      <div class="row">
-
-        <div class="col-lg-12 col-12">
-
-          <h2 class="mb-5 text-center" data-aos="fade-up">
-            Please take a look through our
-            <strong>featured Digital Trends</strong>
-          </h2>
-
-          <div class="owl-carousel owl-theme" id="project-slide">
-            <div class="item project-wrapper" data-aos="fade-up" data-aos-delay="100">
-              <img src="images/project/project-image01.jpg" class="img-fluid" alt="project image">
-
-              <div class="project-info">
-                <small>Marketing</small>
-
-                <h3>
-                  <a href="project-detail.html">
-                    <span>Sweet Go Agency</span>
-                    <i class="fa fa-angle-right project-icon"></i>
-                  </a>
-                </h3>
-              </div>
-            </div>
-
-            <div class="item project-wrapper" data-aos="fade-up">
-              <img src="images/project/project-image02.jpg" class="img-fluid" alt="project image">
-
-              <div class="project-info">
-                <small>Website</small>
-
-                <h3>
-                  <a href="project-detail.html">
-                    <span>Smart Ladies</span>
-                    <i class="fa fa-angle-right project-icon"></i>
-                  </a>
-                </h3>
-              </div>
-            </div>
-
-            <div class="item project-wrapper" data-aos="fade-up">
-              <img src="images/project/project-image03.jpg" class="img-fluid" alt="project image">
-
-              <div class="project-info">
-                <small>Branding</small>
-
-                <h3>
-                  <a href="project-detail.html">
-                    <span>Shoes factory</span>
-                    <i class="fa fa-angle-right project-icon"></i>
-                  </a>
-                </h3>
-              </div>
-            </div>
-
-            <div class="item project-wrapper" data-aos="fade-up">
-              <img src="images/project/project-image04.jpg" class="img-fluid" alt="project image">
-
-              <div class="project-info">
-                <small>Social Media</small>
-
-                <h3>
-                  <a href="project-detail.html">
-                    <span>Race Bicycle</span>
-                    <i class="fa fa-angle-right project-icon"></i>
-                  </a>
-                </h3>
-              </div>
-            </div>
-
-            <div class="item project-wrapper" data-aos="fade-up">
-              <img src="images/project/project-image05.jpg" class="img-fluid" alt="project image">
-
-              <div class="project-info">
-                <small>Video</small>
-
-                <h3>
-                  <a href="project-detail.html">
-                    <span>Ultimate HealthCare</span>
-                    <i class="fa fa-angle-right project-icon"></i>
-                  </a>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </section>
-
 
   <!-- TESTIMONIAL -->
   <section class="testimonial section-padding">
